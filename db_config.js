510f872fafbf9a4b19121b0d59b4e4a5e7b85914
mayank <mayankyadavdotcom@gmail.com>
2023-09-06 06:44:16 +0530

    Second commit

--- db_config.js
+++ db_config.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 async function db_connect() {
   console.log("running db_connect");
   try {
-    await mongoose.connect(process.env.Mongo_url, { dbName: "IMS" });
+    await mongoose.connect(
+      "mongodb+srv://BhaalaDev:Stick1with2me@bhaaladev.irefmnr.mongodb.net/?retryWrites=true&w=majority",
+      { dbName: "IMS" }
+    );
     console.log("Connection Successfull");
   } catch (error) {
     console.log("Error in connecting DB");
